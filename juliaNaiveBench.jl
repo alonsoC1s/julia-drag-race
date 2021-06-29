7f8d6efd1c251fb28b0853760cf0f6350ee5092f
Alonso Martinez <alonso.mtz99@gmail.com>
2021-06-29 14:25:37 -0500

    Formating

--- juliaNaiveBench.jl
+++ juliaNaiveBench.jl
@@ -4,42 +4,39 @@ using .pivotOperations, Random, DataFrames, CSV
 
 # Creating DataFrame to store results
 function benchmarkNaive(reps)
-    raw = DataFrame(lang = String[],
-	    method = String[],
-		dim = Int[],
-		time = Float64[])
+    raw = DataFrame(lang = String[], method = String[], dim = Int[], time = Float64[])
 
-		allFunctions = [
-			pivotGauss,
-			pivotSimple,
-			pivotHybrid,
-			pivotSimpleInbounds,
-			pivotSimpleParallel,
-			pivotSimpleView,
-			pivotSimpleView!,
-			pivotGaussNew,
-			pivotGaussplainfor
-		]
+    allFunctions = [
+        pivotGauss,
+        pivotSimple,
+        pivotHybrid,
+        pivotSimpleInbounds,
+        pivotSimpleParallel,
+        pivotSimpleView,
+        pivotSimpleView!,
+        pivotGaussNew,
+        pivotGaussplainfor,
+    ]
 
-    @inbounds for dim in 100:100:1500
+    @inbounds for dim = 100:100:1500
 
-		@inbounds for pivFunc in allFunctions
+        @inbounds for pivFunc in allFunctions
 
-			# Seed a MersenneTwister to generate a random matrix of specified dimensions
-			A = rand(MersenneTwister(98), float(collect(-100:100)), (dim, dim))
+            # Seed a MersenneTwister to generate a random matrix of specified dimensions
+            A = rand(MersenneTwister(98), float(collect(-100:100)), (dim, dim))
 
-			# Use the same Twister to generate a random index to the matrix
-			idx = rand(MersenneTwister(98), 1:dim, 2)
+            # Use the same Twister to generate a random index to the matrix
+            idx = rand(MersenneTwister(98), 1:dim, 2)
 
-			# Actual benchmarking
-			@inbounds for iter = 1:reps
-				# I am using @elepsed instead of @belapsed deliberately
-				pivTime = @elapsed pivFunc(A, idx...)
-				println("dim:$dim, $(string(pivFunc)) rep:$iter en $pivTime s")
+            # Actual benchmarking
+            @inbounds for iter = 1:reps
+                # I am using @elepsed instead of @belapsed deliberately
+                pivTime = @elapsed pivFunc(A, idx...)
+                println("dim:$dim, $(string(pivFunc)) rep:$iter en $pivTime s")
 
-				push!(raw, ["Julia", string(pivFunc), dim, pivTime])
-			end
-		end
+                push!(raw, ["Julia", string(pivFunc), dim, pivTime])
+            end
+        end
 
     end
 
--- pivotOperations.jl
+++ pivotOperations.jl
@@ -13,27 +13,27 @@ Performs a pivot operation on `A` via multiplication by elementary matrices. See
 https://en.wikipedia.org/wiki/Elementary_matrix
 """
 function pivotGauss(A, i, j)
-	m, _ = size(A)
+    m, _ = size(A)
 
-	# First we divide the pivot row by the pivot value so the pivot (A[i, j]) becomes a principal 1.
-	# The idea is to perform every operation on the matrix as a matrix product.
-	# d is a vector that will be used later to create a diagonal matrix.
-	d = vec([ones(i-1, 1); A[i,j].^-1; ones(m-i, 1)])
-	D = Matrix(Diagonal(d))
-	# The product D * A performs the division (yes I realize I could use ./)
-	A = D * A
+    # First we divide the pivot row by the pivot value so the pivot (A[i, j]) becomes a principal 1.
+    # The idea is to perform every operation on the matrix as a matrix product.
+    # d is a vector that will be used later to create a diagonal matrix.
+    d = vec([ones(i - 1, 1); A[i, j] .^ -1; ones(m - i, 1)])
+    D = Matrix(Diagonal(d))
+    # The product D * A performs the division (yes I realize I could use ./)
+    A = D * A
 
-	# Getting the multiples such that R[:, j] - efes .* A[i, j] = 0
-	efes = A[:, j]
+    # Getting the multiples such that R[:, j] - efes .* A[i, j] = 0
+    efes = A[:, j]
 
-	# Allocating an identity matrix that will be modified to become elementary.
-	E = Matrix(1.0 * I(m)) # Hack to make it a numeric (not bool) matrix
-	# Modifying E to make it elementary and perform the desired operation
-	E[:, i] = -efes
-	E[i, :] = -E[i, :] # Ugly. Corrects sign error so thet we don't get a -1
+    # Allocating an identity matrix that will be modified to become elementary.
+    E = Matrix(1.0 * I(m)) # Hack to make it a numeric (not bool) matrix
+    # Modifying E to make it elementary and perform the desired operation
+    E[:, i] = -efes
+    E[i, :] = -E[i, :] # Ugly. Corrects sign error so thet we don't get a -1
 
-	# Returns the result of the product wich is the pivoted matrix
-	return E * A
+    # Returns the result of the product wich is the pivoted matrix
+    return E * A
 end
 
 
@@ -44,24 +44,24 @@ elementwise division and the rest of the pivoting operation (creating zeros abov
 acomplished with a matrix product by an elementary matrix.
 """
 function pivotHybrid(A, i, j)
-	m, _ = size(A)
+    m, _ = size(A)
 
-	# Dividing the row to get the principal 1.
-	A[i, :] = A[i, :] ./ A[i, j]
+    # Dividing the row to get the principal 1.
+    A[i, :] = A[i, :] ./ A[i, j]
 
-	# Getting the multiples such that R[:, j] - efes .* A[i, j] = 0
-	efes = A[:, j]
+    # Getting the multiples such that R[:, j] - efes .* A[i, j] = 0
+    efes = A[:, j]
 
-	# Allocating an identity matrix that will be modified to become elementary.
-	# BLACK MAGIC: Changing the line below from 1 to 1.0 results in DRAMATIC performance differences
-	E = Matrix(1.0 * I(m)) # Hack para que sea matriz numerica no de bools.
+    # Allocating an identity matrix that will be modified to become elementary.
+    # BLACK MAGIC: Changing the line below from 1 to 1.0 results in DRAMATIC performance differences
+    E = Matrix(1.0 * I(m)) # Hack para que sea matriz numerica no de bools.
 
-	# Modifying E to make it elementary and perform the desired operation
-	E[:, i] = -efes
-	E[i, :] = -E[i, :] # Ugly. Corrects sign error so thet we don't get a -1
+    # Modifying E to make it elementary and perform the desired operation
+    E[:, i] = -efes
+    E[i, :] = -E[i, :] # Ugly. Corrects sign error so thet we don't get a -1
 
-	# Returns the result of the product wich is the pivoted matrix
-	return E * A
+    # Returns the result of the product wich is the pivoted matrix
+    return E * A
 end
 
 """
@@ -69,16 +69,16 @@ end
 Performs a pivot operation on `A` using a simple approach: for loops.
 """
 function pivotSimple(A, i, j)
-	# Dividing the row to get the principal 1.
-	A[i, :] = A[i, :] ./ A[i, j]
+    # Dividing the row to get the principal 1.
+    A[i, :] = A[i, :] ./ A[i, j]
 
-	for irow = 1:size(A,1)
-		if irow != i
-			A[irow, :] = A[irow, :] .- A[i, :] .* A[irow, j]
-		end
-	end
+    for irow = 1:size(A, 1)
+        if irow != i
+            A[irow, :] = A[irow, :] .- A[i, :] .* A[irow, j]
+        end
+    end
 
-	return A
+    return A
 end
 
 
@@ -86,7 +86,7 @@ function pivotSimpleView!(A, i, j)
     # Dividing the row to get the principal 1.
     A[i, :] .= @view(A[i, :]) ./ A[i, j]
 
-    for irow = 1:size(A,1)
+    for irow = 1:size(A, 1)
         if irow != i
             A[irow, :] .= @view(A[irow, :]) .- @view(A[i, :]) .* A[irow, j]
         end
@@ -100,7 +100,7 @@ function pivotSimpleView(A, i, j)
     # Dividing the row to get the principal 1.
     A[i, :] = @view(A[i, :]) ./ A[i, j]
 
-    for irow = 1:size(A,1)
+    for irow = 1:size(A, 1)
         if irow != i
             A[irow, :] = @view(A[irow, :]) .- @view(A[i, :]) .* A[irow, j]
         end
@@ -115,15 +115,15 @@ Performs a pivot operation on `A` using a annotated for loops. Intented to test 
 of using @inbounds
 """
 function pivotSimpleInbounds(A, i, j)
-	@inbounds A[i, :] = A[i, :] ./ A[i, j]
+    @inbounds A[i, :] = A[i, :] ./ A[i, j]
 
-	@inbounds for irow = 1:size(A,1)
-		if irow != i
-			@inbounds A[irow, :] = A[irow, :] .- A[i, :] .* A[irow, j]
-		end
-	end
+    @inbounds for irow = 1:size(A, 1)
+        if irow != i
+            @inbounds A[irow, :] = A[irow, :] .- A[i, :] .* A[irow, j]
+        end
+    end
 
-	return A
+    return A
 end
 
 """
@@ -131,15 +131,15 @@ end
 Performs a pivot operation on `A` using for loops, @inbounds annotations, and multithreading.
 """
 function pivotSimpleParallel(A, i, j)
-	@inbounds A[i, :] = A[i, :] ./ A[i, j]
+    @inbounds A[i, :] = A[i, :] ./ A[i, j]
 
-	@threads for irow = 1:size(A, 1)
-		if irow != i
-			@inbounds A[irow, :] = A[irow, :] .- A[i, :] .* A[irow, j]
-		end
-	end
+    @threads for irow = 1:size(A, 1)
+        if irow != i
+            @inbounds A[irow, :] = A[irow, :] .- A[i, :] .* A[irow, j]
+        end
+    end
 
-	return A
+    return A
 end
 
 function pivotGaussNew(A::Matrix{Float64}, i::Int, j::Int)
@@ -147,7 +147,7 @@ function pivotGaussNew(A::Matrix{Float64}, i::Int, j::Int)
     # First we divide the pivot row by the pivot value so the pivot (A[i, j]) becomes a principal 1.
     # The idea is to perform every operation on the matrix as a matrix product.
     # d is a vector that will be used later to create a diagonal matrix.
-    d = vec([ones(i-1, 1); A[i,j]^-1; ones(m-i, 1)])
+    d = vec([ones(i - 1, 1); A[i, j]^-1; ones(m - i, 1)])
     D = Matrix(Diagonal(d))
     # The product D * A performs the division (yes I realize I could use ./)
     A = D * A
@@ -166,25 +166,25 @@ function pivotGaussNew(A::Matrix{Float64}, i::Int, j::Int)
 end
 
 
-function pivotGaussplainfor(A::Matrix{Float64}, i::Int, j::Int)
+function pivotPlainFor(A::Matrix{Float64}, i::Int, j::Int)
     n, m = size(A)
     M = copy(A)
-    p = M[i,j]
+    p = M[i, j]
     for l = 1:m
-        M[i,l] = M[i,l]/p
+        M[i, l] = M[i, l] / p
     end
     for l = 1:m
         if l != j
             for k = 1:n
                 if k != i
-                    M[k,l] = M[k,l] - M[i,l] * M[k,j]
+                    M[k, l] = M[k, l] - M[i, l] * M[k, j]
                 end
             end
         end
     end
     for k = 1:n
         if k != i
-            M[k,j] = 0.0
+            M[k, j] = 0.0
         end
     end
     return M
